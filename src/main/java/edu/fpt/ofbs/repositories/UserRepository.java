c9e6c451e68cc9fd4b209530d38b45137d6725f1
hieunguyen959 <hieunqhe130518@fpt.edu.vn>
2021-06-04 15:50:38 +0700

    hieunq: commit

--- src/main/java/edu/fpt/ofbs/repositories/UserRepository.java
+++ src/main/java/edu/fpt/ofbs/repositories/UserRepository.java
@@ -1,11 +1,13 @@
 package edu.fpt.ofbs.repositories;
 
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
 import org.springframework.stereotype.Repository;
 
 import edu.fpt.ofbs.entities.User;
 
 @Repository
 public interface UserRepository extends JpaRepository<User, Integer>{
-
+	@Query(value = "Select * from users u where u.phone_login = ?1", nativeQuery = true)
+	User findByPhoneNumberLogin(String phoneNumber);
 }
--- src/main/java/edu/fpt/ofbs/service/impl/UserServiceImpl.java
+++ src/main/java/edu/fpt/ofbs/service/impl/UserServiceImpl.java
@@ -1,11 +1,7 @@
 package edu.fpt.ofbs.service.impl;
 
-import java.time.LocalDate;
 import java.util.List;
 import java.util.Optional;
-
-import javax.persistence.EntityManager;
-import javax.persistence.TypedQuery;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
@@ -19,9 +15,6 @@ public class UserServiceImpl implements UserService {
 	@Autowired
 	private UserRepository userRepository;
 
-	@Autowired
-	private EntityManager entityManager;
-
 	@Override
 	public List<User> findAll() {
 		return userRepository.findAll();
@@ -33,14 +26,7 @@ public class UserServiceImpl implements UserService {
 
 	@Override
 	public User findByPhoneNumberLogin(String phoneNumber) {
-		String sql = "Select u from user u where u.phoneLogin = :phoneNumber";
-
-		TypedQuery<User> query = entityManager.createQuery(sql, User.class);
-		query.setParameter("phoneNumber", phoneNumber);
-		if (query.getResultList().size() > 0) {
-			return query.getResultList().get(0);
-		}
-		return null;
+		return userRepository.findByPhoneNumberLogin(phoneNumber);
 	}
 
 	@Override
@@ -48,24 +34,4 @@ public class UserServiceImpl implements UserService {
 		return userRepository.findById(id);
 	}
 
-	@Override
-	public User findUserLogin(String phone, String password) {
-		String sql = "Select u from user u where u.phoneLogin = :phone and u.password = :password";
-
-		TypedQuery<User> query = entityManager.createQuery(sql, User.class);
-		query.setParameter("phone", phone);
-		query.setParameter("password", password);
-		if (query.getResultList().size() > 0) {
-			return query.getResultList().get(0);
-		}
-		return null;
-	}
-
-//	@Override
-//	public User register(User user) {
-//		user.setRoleId(3);
-//		user.setLastModified(java.sql.Date.valueOf(LocalDate.now()));
-//		user.setStatusId(1);
-//		return userRepository.save(user);
-//	}
 }
