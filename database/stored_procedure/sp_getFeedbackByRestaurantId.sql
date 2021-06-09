317879ebb49ddc2b60c591885a6613cf98e950c7
TrangTLH <huyentrangtx86@gmail.com>
2021-06-09 22:45:03 +0700

    trangtlh: update stored procedure getFeedback

--- database/stored_procedure/sp_getFeedbackByRestaurantId.sql
+++ database/stored_procedure/sp_getFeedbackByRestaurantId.sql
@@ -2,7 +2,7 @@ CREATE PROCEDURE sp_getFeedbackByRestaurantId
 	@restaurant_id int
 AS
 BEGIN
-	SELECT fb.id as feedback_id, users.name as user_name, img.id as image_user_id, fb.rate, fb.feedback_content, fb.feedback_date
+	SELECT users.id as user_id, users.name as user_name, img.id as image_user_id, fb.rate, fb.feedback_content, fb.feedback_date, fb.restaurant_id
 	FROM feedbacks fb
 		join users on fb.customer_id = users.id
 		join images img on img.user_id = fb.customer_id
