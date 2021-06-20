2fe90a746a93fef385671d93ccc200f279cf684f
TrangTLH <huyentrangtx86@gmail.com>
2021-06-20 10:08:21 +0700

    trangtlh: add query user and promotion

---
+++ database/stored_procedure/sp_getUserProfileById.sql
@@ -0,0 +1,10 @@
+CREATE PROC sp_getUserProfileById @user_id int
+AS
+BEGIN
+	SELECT users.name as user_name, images.id as image_id, users.email, users.phone_number, users.gender, 
+		users.date_of_birth, users.address
+	FROM users	
+		left join images on users.id = images.user_id
+	WHERE users.id  = @user_id
+END
+
