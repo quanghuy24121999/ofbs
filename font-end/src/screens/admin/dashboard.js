756573e5367a03d01e0f327b7a13d83eb6329d54
quanghuy24121999 <quanghuy24121999@gmail.com>
2021-07-03 18:10:31 +0700

    huynq: update chart

--- font-end/src/screens/admin/dashboard.js
+++ font-end/src/screens/admin/dashboard.js
@@ -1,25 +1,58 @@
-import React, { useState } from 'react';
+import React, { useEffect, useState } from 'react';
 import SlideBar from '../../components/admin/SlideBar';
 import { FaBars, FaSearch } from 'react-icons/fa';
 import { Link } from 'react-router-dom';
 import { Container, Row, Col, Input, Button, Label } from 'reactstrap';
-import { Bar, Pie } from 'react-chartjs-2';
+import { Pie } from 'react-chartjs-2';
+import axios from 'axios';
+
+import { formatDateForInput } from '../../common/formatDate';
 
 function Dashboard() {
     const [toggled, setToggled] = useState(false);
-    const [from, setFrom] = useState('');
-    const [to, setTo] = useState('');
+    const [from, setFrom] = useState(formatDateForInput(new Date()));
+    const [to, setTo] = useState(formatDateForInput(new Date()));
+    const [pending, setPending] = useState(0);
+    const [preparing, setPreparing] = useState(0);
+    const [accomplished, setAccomplished] = useState(0);
+    const [cancelled, setCancelled] = useState(0);
 
     const onChangeFrom = (e) => {
         setFrom(e.target.value);
     }
 
+    useEffect(() => {
+        search();
+    })
+
     const onChangeTo = (e) => {
         setTo(e.target.value);
     }
 
-    const search = () => {
+    const getTotalOrder = (status) => {
+        axios.get(`/orders/getTotalOrderByStatus?status=${status}&fromDate=${from}&toDate=${to}`, {
+            headers: {
+                'Authorization': 'Bearer ' + localStorage.getItem('token')
+            }
+        })
+            .then(res => {
+                if (status === 'pending') {
+                    setPending(res.data);
+                } else if (status === 'preparing') {
+                    setPreparing(res.data);
+                } else if (status === 'accomplished') {
+                    setAccomplished(res.data);
+                } else if (status === 'cancelled') {
+                    setCancelled(res.data);
+                }
+            })
+    }
 
+    const search = () => {
+        getTotalOrder('pending');
+        getTotalOrder('preparing');
+        getTotalOrder('accomplished');
+        getTotalOrder('cancelled');
     }
 
     const handleToggleSidebar = (value) => {
@@ -48,44 +81,48 @@ function Dashboard() {
                     <Row className="dashboard-row">
                         <Col lg="8" md="12" sm="12">
                             <div>
-                                <Pie
+                                {(pending !== 0 && preparing !==0 && accomplished !==0
+                                && cancelled !== 0) ? < Pie
                                     id="myChart"
-                                    data={{
-                                        labels: ["Đang chờ duyệt", "Chưa diễn ra", "Đã hoàn thành", "Đã hủy"],
-                                        datasets: [
-                                            {
-                                                label: ["Đơn hàng"],
-                                                data: [1, 5, 9, 10],
-                                                backgroundColor: [
-                                                    '#831ae4d9',
-                                                    '#5771cfd9',
-                                                    '#12c32fd9',
-                                                    '#db4646d9'
-                                                ],
-                                                borderColor: [
-                                                    '#831ae4d9',
-                                                    '#5771cfd9',
-                                                    '#12c32fd9',
-                                                    '#db4646d9'
-                                                ],
-                                                borderWidth: 1,
-                                            }
-                                        ]
-                                    }}
-                                    width={50}
-                                    height={300}
-                                    options={{
-                                        maintainAspectRatio: false,
-                                        responsive: true,
-                                        // scales: {
-                                        //     yAxes: [{
-                                        //         ticks: {
-                                        //             beginAtZero: true
-                                        //         }
-                                        //     }]
-                                        // }
-                                    }}
-                                />
+                                data={{
+                                    labels: ["Đang chờ duyệt", "Chưa diễn ra", "Đã hoàn thành", "Đã hủy"],
+                                    datasets: [
+                                        {
+                                            label: ["Đơn hàng"],
+                                            data: [pending, preparing, accomplished, cancelled],
+                                            backgroundColor: [
+                                                '#831ae4d9',
+                                                '#5771cfd9',
+                                                '#12c32fd9',
+                                                '#db4646d9'
+                                            ],
+                                            borderColor: [
+                                                '#831ae4d9',
+                                                '#5771cfd9',
+                                                '#12c32fd9',
+                                                '#db4646d9'
+                                            ],
+                                            borderWidth: 1,
+                                        }
+                                    ]
+                                }}
+                                width={50}
+                                height={300}
+                                options={{
+                                    maintainAspectRatio: false,
+                                    responsive: true,
+                                    // scales: {
+                                    //     yAxes: [{
+                                    //         ticks: {
+                                    //             beginAtZero: true
+                                    //         }
+                                    //     }]
+                                    // }
+                                }}
+                                /> : <p className="chart-title">
+                                    Không có đơn hàng nào trong khoảng thời gian này !
+                                </p>
+                            }
                             </div>
                             <p className="chart-title">Thống kê đơn hàng theo thời gian</p>
                         </Col>
@@ -108,9 +145,9 @@ function Dashboard() {
                                     onChange={onChangeTo}
                                 />
                             </div>
-                            <Button className="btn-search-chart" color="success" onClick={() => search()}>
+                            {/* <Button className="btn-search-chart" color="success" onClick={() => search()}>
                                 <FaSearch />
-                            </Button>
+                            </Button> */}
                         </Col>
                     </Row>
                 </Container>
