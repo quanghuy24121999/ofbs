9eaa88baff69de412d203afbc0045673c0fe1de9
quanghuy24121999 <quanghuy24121999@gmail.com>
2021-07-09 10:05:06 +0700

    huynq: update cart

--- font-end/src/components/restaurant/cartServiceItem.js
+++ font-end/src/components/restaurant/cartServiceItem.js
@@ -15,7 +15,7 @@ export default function CartServiceItem(props) {
     const [quantity, setQuantity] = useState(item.quantity);
 
     const onChangeQuantity = () => {
-        const e = document.getElementById('input-quantity-' + item.id);
+        const e = document.getElementById('input-quantity-' + item.image_service_id);
         item.quantity = parseInt(e.value);
 
         setQuantity(item.quantity);
@@ -61,7 +61,7 @@ export default function CartServiceItem(props) {
                         </Button>
                         <div className="cart-dish-quantity">
                             <Input
-                                id={`input-quantity-${item.id}`}
+                                id={`input-quantity-${item.image_service_id}`}
                                 className="cart-input-quantity"
                                 type="number"
                                 min={0}
