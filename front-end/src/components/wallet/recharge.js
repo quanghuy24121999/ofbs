7b6cfb8dfe0bdfa5287df5622ab8d3fb428c7bcb
quanghuy24121999 <quanghuy24121999@gmail.com>
2021-08-06 10:41:12 +0700

    huynq: paypal update

--- front-end/src/components/wallet/recharge.js
+++ front-end/src/components/wallet/recharge.js
@@ -4,14 +4,10 @@ import {
     FormGroup, Label, Modal, ModalHeader,
     ModalBody, ModalFooter
 } from 'reactstrap';
+import { PayPalButton } from "react-paypal-button-v2";
 import { Notify } from '../../common/notify';
 import { api } from '../../config/axios';
 import Spinner from '../common/spinner';
-
-// import firebase from "../../config/firebase";
-
-// let recap = null;
-// let event = '';
 
 export default function Recharge() {
     const [money, setMoney] = useState('');
@@ -26,57 +22,21 @@ export default function Recharge() {
         setMoney(e.target.value)
     }
 
-    // const onChangeOtp = (e) => {
-    //     setOtp(e.target.value)
-    // }
-
     const showContent = (type) => {
-        // if (type === 0) {
-        //     document.getElementById('address').style.display = 'flex';
-        //     document.getElementById('bank-info').style.display = 'none';
         if (type === 1) {
             document.getElementById('bank-info').style.display = 'flex';
-            // document.getElementById('address').style.display = 'none';
+            document.getElementById('recharge-btn').style.display = 'block';
+            document.getElementById('paypal-btn').style.display = 'none';
         } else if (type === 2) {
             document.getElementById('bank-info').style.display = 'none';
-            // document.getElementById('address').style.display = 'none';
+            document.getElementById('recharge-btn').style.display = 'none';
+            document.getElementById('paypal-btn').style.display = 'block';
         }
     }
 
 
     const paypalChechout = () => {
         let description = '';
-        // if (active === 0) {
-        //     description = 'Nạp tiền vào ví FBS - Tiền mặt';
-        //     api.get(`/users/findByPhoneNumber/${localStorage.getItem("currentUser")}`)
-        //         .then(res => {
-        //             const currentUser = res.data;
-        //             api.post(`/payment/save`,
-        //                 {
-        //                     "user": currentUser,
-        //                     "fromToUser": currentUser,
-        //                     "balanceChange": parseFloat(money),
-        //                     "currentBalance": parseFloat(currentUser.balance) + parseFloat(money),
-        //                     "description": description,
-        //                     "paymentType": {
-        //                         "name": "charge"
-        //                     }
-        //                 },
-        //                 {
-        //                     headers: {
-        //                         'Authorization': 'Bearer ' + localStorage.getItem('token')
-        //                     }
-        //                 }
-        //             ).then(res => {
-        //                 Notify('Yêu cầu nạp tiền của bạn đã được gửi lên hệ thống, chúng tôi sẽ xem xét và xử lý sớm nhất',
-        //                     'success', 'top-right'
-        //                 );
-        //                 toggle();
-        //                 setMoney(0);
-        //             })
-
-        //         })
-        // } 
         if (active === 1) {
             description = 'Nạp tiền vào ví FBS - Chuyển khoản qua ngân hàng';
             api.get(`/users/findByPhoneNumber/${localStorage.getItem("currentUser")}`)
@@ -106,101 +66,97 @@ export default function Recharge() {
                         toggle();
                     })
                 })
-        } else {
-            description = 'Nạp tiền vào ví FBS - Ví Paypal';
-            api.get(`/users/findByPhoneNumber/${localStorage.getItem("currentUser")}`)
-                .then(res => {
-                    const currentUser = res.data;
-                    api.post(`/payment/save`,
-                        {
-                            "user": currentUser,
-                            "fromToUser": currentUser,
-                            "balanceChange": money,
-                            "currentBalance": parseFloat(currentUser.balance) + parseFloat(money),
-                            "description": description,
-                            "paymentType": {
-                                "name": "charge"
-                            }
-                        },
-                        {
-                            headers: {
-                                'Authorization': 'Bearer ' + localStorage.getItem('token')
-                            }
-                        }
-                    ).then(res => {
-                        localStorage.setItem("paymentHistoryId", res.data.id);
-                        localStorage.setItem("paymentType", res.data.paymentType.name);
-
-                        api({
-                            method: 'POST',
-                            url: `/payment/pay?price=${money / 23000}&description=${'Nạp tiền vào ví FBS'}`,
-                            headers: {
-                                'Authorization': 'Bearer ' + localStorage.getItem('token')
-                            }
-                        })
-                            .then(res => {
-                                setLoading(false);
-                                window.location.replace(res.data);
-                            }).catch(err => {
-                                console.log(err);
-                            });
-                    })
-                })
         }
+        // else {
+        //     description = 'Nạp tiền vào ví FBS - Ví Paypal';
+        //     api.get(`/users/findByPhoneNumber/${localStorage.getItem("currentUser")}`)
+        //         .then(res => {
+        //             const currentUser = res.data;
+        //             api.post(`/payment/save`,
+        //                 {
+        //                     "user": currentUser,
+        //                     "fromToUser": currentUser,
+        //                     "balanceChange": money,
+        //                     "currentBalance": parseFloat(currentUser.balance) + parseFloat(money),
+        //                     "description": description,
+        //                     "paymentType": {
+        //                         "name": "charge"
+        //                     }
+        //                 },
+        //                 {
+        //                     headers: {
+        //                         'Authorization': 'Bearer ' + localStorage.getItem('token')
+        //                     }
+        //                 }
+        //             ).then(res => {
+        //                 localStorage.setItem("paymentHistoryId", res.data.id);
+        //                 localStorage.setItem("paymentType", res.data.paymentType.name);
+
+        //                 api({
+        //                     method: 'POST',
+        //                     url: `/payment/pay?price=${money / 23000}&description=${'Nạp tiền vào ví FBS'}`,
+        //                     headers: {
+        //                         'Authorization': 'Bearer ' + localStorage.getItem('token')
+        //                     }
+        //                 })
+        //                     .then(res => {
+        //                         setLoading(false);
+        //                         window.location.replace(res.data);
+        //                     }).catch(err => {
+        //                         console.log(err);
+        //                     });
+        //             })
+        //         })
+        // }
     }
 
-    // const [modal, setModal] = useState(false);
-
-    // const checkCaptcha = () => {
-    //     let phone = '+84368020200';
-
-    //     if (!modal) {
-    //         let recapcha = new firebase.auth.RecaptchaVerifier("recaptcha");
-    //         firebase.auth().signInWithPhoneNumber(phone, recapcha)
-    //             .then(function (e) {
-    //                 recap = recapcha;
-    //                 event = e;
-
-    //                 toggle();
-    //                 document.getElementById('verify-code').style.display = "flex";
-    //                 document.getElementById('recaptcha').style.display = "none";
-    //                 document.getElementById('btn-recharge').style.display = "flex";
-    //             }).catch((error) => {
-    //                 recapcha.clear();
-    //                 console.log(error);
-    //                 Notify("Lỗi hệ thống !", "error", "top-right");
-    //             });
-    //     }
-    // }
-
-    // const toggle = () => {
-    //     setModal(!modal);
-    // };
-
-    // const recharge = (recapcha, e) => {
-    //     if (otp === '' || otp === null) {
-    //         Notify("Vui lòng nhập mã OTP !", "error", "top-right");
-    //         return;
-    //     } else {
-    //         e.confirm(otp)
-    //             .then(function (result) {
-    //                 // api.post('')
-    //                 //     .then(res => {
-    //                 //         Notify("Đổi mật khẩu thành công !", "success", "top-right");
-    //                 //         recapcha.clear();
-    //                 //         window.location.reload();
-    //                 //     })
-    //                 Notify("Đổi mật khẩu thành công !", "success", "top-right");
-    //                 toggle();
-    //             })
-    //             .catch((error) => {
-    //                 console.log(error);
-    //                 Notify("Mã OTP không chính xác !", "error", "top-right");
-    //                 recapcha.clear();
-    //                 window.location.reload();
-    //             });
-    //     }
-    // }
+    const paypal = (status) => {
+        api.get(`/users/findByPhoneNumber/${localStorage.getItem("currentUser")}`)
+            .then(res => {
+                const currentUser = res.data;
+                api.post(`/payment/save`,
+                    {
+                        "user": currentUser,
+                        "fromToUser": currentUser,
+                        "balanceChange": money,
+                        "currentBalance": parseFloat(currentUser.balance) + parseFloat(money),
+                        "description": 'Nạp tiền vào ví FBS - Ví Paypal',
+                        "paymentType": {
+                            "name": "charge"
+                        }
+                    },
+                    {
+                        headers: {
+                            'Authorization': 'Bearer ' + localStorage.getItem('token')
+                        }
+                    }
+                ).then(res => {
+                    const balanceChange = res.data.balanceChange;
+                    api({
+                        method: 'PATCH',
+                        headers: {
+                            'Authorization': 'Bearer ' + localStorage.getItem('token')
+                        },
+                        url: `/payment/updateStatus?paymentId=${res.data.id}&status=${status}`
+                    }).then(res => {
+                        api({
+                            method: 'PATCH',
+                            headers: {
+                                'Authorization': 'Bearer ' + localStorage.getItem('token')
+                            },
+                            url: `users/updateBalance?balance=${parseFloat(currentUser.balance) + parseFloat(balanceChange)}&userId=${currentUser.id}`
+                        }).then(() => {
+                            if (status === 'success') {
+                                Notify('Nạp tiền vào ví thành công', 'success', 'top-right');
+                            } else if (status === 'fail') {
+                                Notify('Nạp tiền vào ví không thành công', 'error', 'top-right');
+                            }
+                            setMoney('');
+                        })
+                    })
+                })
+            })
+    }
 
     return (
         <Container className="wallet-recharge">
@@ -209,24 +165,6 @@ export default function Recharge() {
                     <FormGroup tag="fieldset" className="recharge">
                         <div className="recharge-title">Chọn phương thức nạp tiền</div>
                         <hr />
-                        {/* <FormGroup check>
-                            <Label check>
-                                <Input
-                                    type="radio"
-                                    name="radio1"
-                                    checked={active === 0}
-                                    onClick={() => {
-                                        setActive(0);
-                                        showContent(0);
-                                    }}
-                                />{' '}
-                                <span className="type-recharge">Nạp tiền bằng tiền mặt</span>
-                            </Label>
-                            <ul id="address">
-                                <li><span className="bank-info-title">Địa chỉ: </span>Khu công nghệ cao Hòa Lạc – Km29, ĐCT08, Thạch Hoà, Thạch Thất, Hà Nội</li>
-                                <li><span className="bank-info-title">Số điện thoại:</span> 0368020200</li>
-                            </ul>
-                        </FormGroup> */}
                         <FormGroup check>
                             <Label check>
                                 <Input
@@ -276,7 +214,23 @@ export default function Recharge() {
                         min={10000}
                         placeholder="Nhập số tiền muốn nạp"
                     />
-                    <Button color="success" onClick={() => {
+                    <div id="paypal-btn" style={{ zIndex: '1', display: 'none' }}>
+                        <PayPalButton
+                            options={{
+                                clientId: "AQaSaF02zb7zZbXZIlDArwaUY4L2RTY7NfZwoils6_dfoKGDO10lpJPe3zhe-X6qKVHCASPiOp4rIkNS",
+                                currency: "USD"
+                            }}
+                            amount={parseFloat(money / 23000).toFixed(2)}
+                            onSuccess={(details, data) => {
+                                paypal('success');
+                            }}
+                            onCancel={() => {
+                                paypal('fail');
+                            }}
+                        />
+                    </div>
+
+                    <Button id="recharge-btn" color="success" onClick={() => {
                         if (parseFloat(money) > 0 && money !== '') {
                             if (parseFloat(money) >= 10000) {
                                 toggle();
@@ -301,27 +255,6 @@ export default function Recharge() {
                             <Button color="secondary" onClick={toggle}>Quay lại</Button>
                         </ModalFooter>
                     </Modal>
-                    {/* <div id="recaptcha"></div> */}
-                    {/* <Modal isOpen={modal} toggle={toggle} className={``}>
-                    <ModalHeader toggle={toggle}>Thông báo</ModalHeader>
-                    <ModalBody>
-                        <div id="verify-code">
-                            <b><span className="require-icon">*</span></b>
-                            <Input
-                                type="text"
-                                id="code"
-                                value={otp}
-                                onChange={onChangeOtp}
-                                required="required"
-                                placeholder="Nhập mã OTP"
-                            />
-                        </div>
-                    </ModalBody>
-                    <ModalFooter>
-                        <Button id="btn-recharge" color="success" onClick={() => recharge(recap, event)}>Đồng ý</Button>{' '}
-                        <Button color="secondary" onClick={toggle}>Quay lại</Button>
-                    </ModalFooter>
-                </Modal> */}
                 </Col>
             </Row>
         </Container>
